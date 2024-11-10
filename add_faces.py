677eb27c4821f61de8c101f567d4566d2c8e9322
umehabiba139 <habiba.ume2424@gmail.com>
2024-11-10 16:28:50 +0500

    open web cam by name

--- add_faces.py
+++ add_faces.py
@@ -37,11 +37,28 @@ faces_data=np.asarray(faces_data)
 faces_data=faces_data.reshape(100, -1)
 
 if 'names.pkl' not in os.listdir('data/'):
-   names=name*100
-   with open ('data/names.pkl') as f:
+   names=[name]*100
+   with open ('data/names.pkl','wb' ) as f:
       pickle.dump(names, f)
       
-   
+else:
+   with open ('data/names.pkl','rb') as f:
+      names=pickle.load(f)
+   names=names+[name]*100
+   with open ('data/names.pkl','wb') as f:
+      pickle.dump(names, f)
+
+if 'faces_data.pkl' not in os.listdir('data/'):
+   with open ('data/faces_data.pkl','wb') as f:
+      pickle.dump(faces_data, f)
+      
+else:
+   with open ('data/faces_data.pkl','rb') as f:
+      faces=pickle.load(f)
+   faces=np.append(faces,faces_data, axis=0)
+   with open ('data/faces_data.pkl','wb') as f:
+      pickle.dump(names, f)
+     
 
 
 
